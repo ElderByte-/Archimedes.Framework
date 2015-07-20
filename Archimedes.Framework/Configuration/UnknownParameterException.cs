b102c351671e0fe995fd9f793388005be222b9f5
IsNull <pascalbuettiker@bluewin.ch>
2015-07-20 14:50:19 +0200

    initial commit of code base

---
+++ Archimedes.Framework/Configuration/UnknownParameterException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Archimedes.Framework.Configuration
+{
+    public class UnknownParameterException : Exception
+    {
+        public UnknownParameterException(string parameter)
+            : base("The requiered parameter '" + parameter  + "' was not specified in the App settings!")
+        {
+            
+        }
+    }
+}
+ 
---
+++ Archimedes.Framework/Configuration/Values/ValueAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Archimedes.Framework.AOP
+{
+    /// <summary>
+    /// This attribute provides the ability to inject configuration values into fields.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ValueAttribute : Attribute
+    {
+        public ValueAttribute(string expression)
+        {
+            Expression = expression;
+        }
+
+        public string Expression { get; private set; }
+    }
+}
