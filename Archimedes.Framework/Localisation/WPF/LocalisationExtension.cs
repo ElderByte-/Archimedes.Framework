3eb5d0eaa56ed050bb7e62568e06dd0f92104c80
IsNull <pascalbuettiker@bluewin.ch>
2015-09-29 14:49:57 +0200

    support WPF localisation, simple LocalisationExtension

---
+++ Archimedes.Framework/Localisation/WPF/LocalisationExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Markup;
+using Archimedes.Framework.Context;
+using Archimedes.Framework.Stereotype;
+
+namespace Archimedes.Framework.Localisation.WPF
+{
+    public class LocalisationExtension : MarkupExtension
+    {
+
+        public LocalisationExtension()
+        {
+            
+        }
+
+        public LocalisationExtension(object id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// The text-key for which the translation text should be loaded
+        /// </summary>
+        [ConstructorArgument("id")]
+        public object Id { get; set; }
+
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (!IsInDesignMode())
+            {
+                var localisationService = ApplicationContext.Instance.Container.Resolve<ILocalisationService>();
+                if (localisationService != null)
+                {
+                    return localisationService.GetTranslation(Id.ToString());
+                }
+            }
+
+            return Id;
+        }
+
+        private static bool? _isInDesignMode;
+        public static bool IsInDesignMode()
+        {
+            if (!_isInDesignMode.HasValue)
+            {
+                _isInDesignMode = System.Reflection.Assembly.GetExecutingAssembly().Location.Contains("VisualStudio");
+            }
+            return _isInDesignMode.Value;
+        }
+    }
+}
