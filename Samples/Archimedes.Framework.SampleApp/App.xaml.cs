ee6a261b5df899c61bda7ede3aed7fb980949cea
IsNull <pascalbuettiker@bluewin.ch>
2015-09-29 14:50:26 +0200

    show new localisation feature in Sampler App

--- Samples/Archimedes.Framework.SampleApp/App.xaml.cs
+++ Samples/Archimedes.Framework.SampleApp/App.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
 using Archimedes.Framework.Context;
 using Archimedes.Framework.ContextEnvironment.Properties;
 using Archimedes.Framework.SampleApp.Util;
 using Archimedes.Framework.SampleApp.ViewModels;
 using Archimedes.Framework.Stereotype;
+using log4net;
 
 namespace Archimedes.Framework.SampleApp
 {
@@ -12,6 +14,9 @@ namespace Archimedes.Framework.SampleApp
     /// </summary>
     public partial class App : Application
     {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+
         [Inject]
         private MainViewModel _mainViewModel;
 
@@ -20,6 +25,8 @@ namespace Archimedes.Framework.SampleApp
             // Setup logging
             new LoggerConfiguration(AppUtil.ApplicaitonBinaryFolder, "Debug");
 
+
+            Log.Info("Loading SampleApp...");
 
             var ctx = ApplicationContext.Instance;
 
@@ -35,9 +42,12 @@ namespace Archimedes.Framework.SampleApp
             ApplicationContext.Instance.Container.Autowire(this);
             // The _mainViewModel field has now been injected.
 
+            Log.Info("Archimedes Framework initialized, starting main window...");
 
-            var main = new MainWindow();
-            main.DataContext = _mainViewModel;
+            var main = new MainWindow
+            {
+                DataContext = _mainViewModel
+            };
 
             main.Show();
 
--- Samples/Archimedes.Framework.SampleApp/ViewModels/CustomersViewModel.cs
+++ Samples/Archimedes.Framework.SampleApp/ViewModels/CustomersViewModel.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using Archimedes.Framework.Localisation;
 using Archimedes.Framework.SampleApp.Model;
+using Archimedes.Framework.SampleApp.Util;
 using Archimedes.Framework.Stereotype;
 
 namespace Archimedes.Framework.SampleApp.ViewModels
@@ -11,17 +15,39 @@ namespace Archimedes.Framework.SampleApp.ViewModels
     public class CustomersViewModel
     {
         private readonly CustomerService _customerService;
+        private readonly ILocalisationService _localisationService;
+        private Customer _currentCustomer;
 
         [Inject]
-        public CustomersViewModel(CustomerService customerService)
+        public CustomersViewModel(CustomerService customerService, ILocalisationService localisationService)
         {
             _customerService = customerService;
+            _localisationService = localisationService;
         }
 
         public IEnumerable<Customer> AllCustomers
         {
             get { return _customerService.FindAll(); }
-        } 
+        }
 
+        public Customer CurrentCustomer
+        {
+            get { return _currentCustomer; }
+            set
+            {
+                _currentCustomer = value;
+            }
+        }
+
+
+        public ICommand ShowDetailCommand
+        {
+            get {
+                return new RelayCommand(x =>
+                {
+                    MessageBox.Show(_localisationService.GetTranslation("customers.detail.message", CurrentCustomer));
+                }
+            ); }
+        }
     }
 }
