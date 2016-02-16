6de42688a6ba0608dc41e2e9feb55822a9837fc6
IsNull <p.buettiker@elderbyte.com>
2016-02-16 17:14:39 +0100

    some unit test tweaks

--- Archimedes.Framework.Test/ConfigurationTest/PropertiesParserTest.cs
+++ Archimedes.Framework.Test/ConfigurationTest/PropertiesParserTest.cs
@@ -7,9 +7,6 @@ namespace Archimedes.Framework.Test.ConfigurationTest
     {
         [TestCase("simpleKey=simpleValue", "simpleKey", "simpleValue")]
         [TestCase("simpleKeyNumber=254", "simpleKeyNumber", "254")]
-        [TestCase("keywithComplexVal=Iamsooo;Coold--&&23412+()&ç", "keywithComplexVal", "Iamsooo;Coold--&&23412+()&ç")]
-        [TestCase("keyWithValue=TooCool=324;asdf", "keyWithValue", "TooCool=324;asdf")]
-
         public void TestParse(string propertyLine, string expectedKey, string expectedValue)
         {
             var source = new PropertiesPropertySource(propertyLine);
@@ -19,6 +16,18 @@ namespace Archimedes.Framework.Test.ConfigurationTest
             Assert.AreEqual(properties.Get(expectedKey), expectedValue);
         }
 
+
+
+        [TestCase("keywithComplexVal=Iamsooo;Coold--&&23412+()&ç", "keywithComplexVal", "Iamsooo;Coold--&&23412+()&ç")]
+        [TestCase("keyWithValue=TooCool=324;asdf", "keyWithValue", "TooCool=324;asdf")]
+        public void TestParse2(string propertyLine, string expectedKey, string expectedValue)
+        {
+            var source = new PropertiesPropertySource(propertyLine);
+
+            var properties = source.Load();
+
+            Assert.AreEqual(properties.Get(expectedKey), expectedValue);
+        }
 
 
     }
